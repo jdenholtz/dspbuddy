68772fc7d7189501d275e52b1cb3e94c1545a2b6
Jacob Denholtz <jdenholtz@email.arizona.edu>
2017-11-10 01:46:52 +0000

    data from file displayed in graph

--- app/controllers/datum_controller.rb
+++ app/controllers/datum_controller.rb
@@ -1,6 +1,16 @@
 class DatumController < ApplicationController
 
   def show
+    
+    @mydata = File.read("./app/assets/images/testdata.txt").split(/[\r\n]+/).map(&:to_f)
+    #@mydata = [1, 2, 3, 4, 3, 2, 1]
+    #puts ""
+    #puts "This Is My Data"
+    #puts ""
+    #puts @mydata.inspect
+    #puts ""
+    #puts "Data Complete"
+    #puts ""
   end
-  
+
 end
