ad968543dbe081041b08eea347c7dfee084e1533
Jacob Denholtz <jdenholtz@email.arizona.edu>
2017-11-15 08:18:56 +0000

    Normalize button added and working

--- app/controllers/datas_controller.rb
+++ app/controllers/datas_controller.rb
@@ -4,10 +4,10 @@ class DatasController < ApplicationController
   def show
     @mydata = File.read("./app/assets/images/testdata.txt").split(/[\r\n]+/).map(&:to_f)
     @mysps = 500
-    puts @mysps.inspect
-    @mydata.extend(DescriptiveStatistics)
-    mean = @mydata.mean
-    @mydata = @mydata.map {|x| x - mean}    
+    #puts @mysps.inspect
+    #@mydata.extend(DescriptiveStatistics)
+    #mean = @mydata.mean
+    #@mydata = @mydata.map {|x| x - mean}    
   end
 
 end
