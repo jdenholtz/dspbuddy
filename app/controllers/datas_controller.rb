b861c3c1a46a07bd207835f91cb35fec3d6d448e
Jacob Denholtz <jdenholtz@email.arizona.edu>
2017-11-17 05:42:08 +0000

    Statistics precalculated to save time with stat toggle

--- app/controllers/datas_controller.rb
+++ app/controllers/datas_controller.rb
@@ -2,12 +2,20 @@ class DatasController < ApplicationController
    require 'descriptive_statistics/safe'
 
   def show
-    @mydata = File.read("./app/assets/images/testdata.txt").split(/[\r\n]+/).map(&:to_f)
-    @mysps = 500
-    #puts @mysps.inspect
-    #@mydata.extend(DescriptiveStatistics)
-    #mean = @mydata.mean
-    #@mydata = @mydata.map {|x| x - mean}    
+    mydata = File.read("./app/assets/images/testdata2.txt").split(/[\r\n]+/)#.map(&:to_f)
+    #@mydata = File.read("./app/assets/images/testdata2.txt")gsub(/\s+/m, ' ').strip.split(" ")
+    mydata.length.times do |index|
+      mydata[index] = mydata[index].split(/\t/).map(&:to_f)
+    end
+    
+    @mysps = 960
+    channelToPlot = 1
+    @plotData = []
+    squares = []
+
+    mydata.length.times do |index|
+      @plotData[index] = mydata[index][channelToPlot -1]
+    end
   end
 
 end
