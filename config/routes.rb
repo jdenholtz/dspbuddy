e96c5c8fcf2fe82efbc23fcb6ca52763a0021aac
Jacob Denholtz <jdenholtz@email.arizona.edu>
2017-11-27 00:33:23 +0000

    Informational landing page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'datasets#index'
+  root 'static_pages#index'
 
   resources :datasets do
     resources :datas, only: [:create, :show]
